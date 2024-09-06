8c5ab1928e6c792c4f168362c63d0780b75b48af
Sharkyjoddd <aditya.2023ug4007@iiitranchi.ac.in>
2024-09-06 11:10:37 +0530

    First change

---
+++ app/src/main/java/com/example/agrobot/Splash_screen.kt
@@ -0,0 +1,37 @@
+package com.example.agrobot
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+@Suppress("DEPRECATION")
+class Splash_screen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash_screen)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        // HERE WE ARE TAKING THE REFERENCE OF OUR IMAGE
+        // SO THAT WE CAN PERFORM ANIMATION USING THAT IMAGE
+        val backgroundImage: ImageView = findViewById(R.id.iv1)
+        val slideAnimation = AnimationUtils.loadAnimation(this, R.anim.fade_in)
+        backgroundImage.startAnimation(slideAnimation)
+        Handler().postDelayed({
+            val intent= Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            this.overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
+            finish()
+        },3000)
+    }
+}
