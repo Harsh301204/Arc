ea2847f3c99e4704e94bd3b9bf7e4ce1a8a42914
Rishi Raj <rishiraj1632@gmail.com>
2024-09-08 09:13:02 +0530

    rishi4

--- app/src/main/java/com/example/agrobot/OurTeam.kt
+++ app/src/main/java/com/example/agrobot/OurTeam.kt
@@ -16,10 +16,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [Website.newInstance] factory method to
+ * Use the [OurTeam.newInstance] factory method to
  * create an instance of this fragment.
  */
-class Website : Fragment() {
+class OurTeam : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -37,18 +37,9 @@ class Website : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_website, container, false)
+        return inflater.inflate(R.layout.fragment_our_team, container, false)
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        val website=view.findViewById<TextView>(R.id.URL)
-        website.setOnClickListener {
-            val intent = Intent(Intent.ACTION_VIEW)
-            intent.data = Uri.parse("https://tech-farm.vercel.app")
-            startActivity(intent)
-        }
-    }
     companion object {
         /**
          * Use this factory method to create a new instance of
@@ -61,7 +52,7 @@ class Website : Fragment() {
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            Website().apply {
+            OurTeam().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
